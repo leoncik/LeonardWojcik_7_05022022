148f217c8c3b24cf27ef7850526d68f802dcb261
Léonard Wojcik <leonardw@tutanota.com>
2022-02-06 11:32:53 +0100

    feat: fetch recipes data - Fetch recipes data (JSON format). - Update webpack version to use dev server.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+import { fetchRecipes } from '../scripts/api/api.js';
+
+// Initialize recipes page
+const initRecipesPage = async () => {
+    const recipesData = await fetchRecipes(
+        'src/data/recipesConverted.json'
+    );
+    console.log(recipesData);
+}
+
+initRecipesPage();
