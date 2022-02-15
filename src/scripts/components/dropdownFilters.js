8be910972e7746a4e9cdfe35996c5755161ba75c
Léonard Wojcik <leonardw@tutanota.com>
2022-02-15 11:49:56 +0100

    fix: change dropdown display system - Replace buttons with text input only and use "mousedown" events instead of "click" events.

--- src/scripts/components/dropdownFilters.js
+++ src/scripts/components/dropdownFilters.js
@@ -1,3 +1,65 @@
+/*
+! NEW VERSION */
+// TODO : close dropdown if menu is open and user clicks outside of It to prevent bugs
+
+const dropdownTriggers = document.querySelectorAll('.dropdown-trigger');
+const textFields = document.querySelectorAll('.text-field');
+
+export const enableDropdown = () => {
+    // Open dropdown
+    for (const iterator of dropdownTriggers) {
+      iterator.addEventListener('mousedown', (e) => {
+        switch (e.target.className) {
+          case 'ingredients__dropdown-trigger dropdown-trigger':
+            document.querySelector('.ingredients__list').classList.add('show');
+            document.querySelector('.ingredients__text-field').classList.add('show');
+            break;
+  
+          case 'appliance__dropdown-trigger dropdown-trigger':
+            document.querySelector('.appliance__list').classList.add('show');
+            document.querySelector('.appliance__text-field').classList.add('show');
+            break;
+  
+          case 'ustensils__dropdown-trigger dropdown-trigger':
+            document.querySelector('.ustensils__list').classList.add('show');
+            document.querySelector('.ustensils__text-field').classList.add('show');
+            break;
+        
+          default:
+            break;
+        }
+      })
+    }
+  
+    // Close dropdown
+    for (const iterator of textFields) {
+      iterator.addEventListener('blur', (e) => {
+        switch (e.target.className) {
+          case 'ingredients__text-field text-field show':
+            document.querySelector('.ingredients__list').classList.remove('show');
+            document.querySelector('.ingredients__text-field').classList.remove('show');
+            break;
+  
+          case 'appliance__text-field text-field show':
+            document.querySelector('.appliance__list').classList.remove('show');
+            document.querySelector('.appliance__text-field').classList.remove('show');
+            break;
+  
+          case 'ustensils__text-field text-field show':
+            document.querySelector('.ustensils__list').classList.remove('show');
+            document.querySelector('.ustensils__text-field').classList.remove('show');
+            break;
+        
+          default:
+            break;
+        }
+      })
+    }
+  }
+
+/*
+! OLD VERSION
+
 // DOM elements
 const listContainers = document.querySelectorAll('.search-options ul');
 
@@ -52,10 +114,13 @@ export const enableDropdown = () => {
         // iterator.addEventListener('blur', closeDropdown(e));
         iterator.addEventListener('blur', () => {
             document.querySelector('.ingredients-search .dropdown-button').value = 'Ingrédients';
+            document.querySelector('.ingredients-search .dropdown-button').type = 'button';
             document.querySelector('.ingredients-search .dropdown-button').classList.remove('search-mode');
             document.querySelector('.appliance-search .dropdown-button').value = 'Appareils';
+            document.querySelector('.appliance-search .dropdown-button').type = 'button';
             document.querySelector('.appliance-search .dropdown-button').classList.remove('search-mode');
             document.querySelector('.ustensils-search .dropdown-button').value = 'Ustensiles';
+            document.querySelector('.ustensils-search .dropdown-button').type = 'button';
             document.querySelector('.ustensils-search .dropdown-button').classList.remove('search-mode');
             document.querySelector('.ingredients-search ul').classList.remove('expanded');
             document.querySelector('.appliance-search ul').classList.remove('expanded');
@@ -63,7 +128,7 @@ export const enableDropdown = () => {
 
         });
     }
-}
+} */
 
 
 export const enableSelectFilter = (e) => {
