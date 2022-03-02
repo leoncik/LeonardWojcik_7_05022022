716a90cdd9131de9a84e73b23c79d5ca991e627c
Léonard Wojcik <leonardw@tutanota.com>
2022-03-02 15:30:52 +0100

    feat: update filter lists after research - Fill filter lists options using filtered recipes after research with main bar.

--- src/scripts/components/searchAlgorithm.js
+++ src/scripts/components/searchAlgorithm.js
@@ -1,14 +1,39 @@
 import { Recipe } from '../classes/Recipe';
+import { FilterList } from '../classes/FilterList';
 import { recipes } from '../../data/recipes';
 import { emptyHtmlElement } from '../utils/helpers';
 
 // NO RESULT MESSAGE
-// TODO : Add Non-breaking space (nbsp).
 const noResultMessage = () => {
     document.querySelector(
         '.results'
     ).innerHTML = `<p class="no-result-message">Aucune recette ne correspond à votre critère... vous pouvez
-  chercher « tarte aux pommes », « poisson », etc.</p>`;
+  chercher «&nbsp;tarte aux pommes&nbsp;», «&nbsp;poisson&nbsp;», etc.</p>`;
+};
+
+// UPDATE FILTERS AFTER MAIN RESEARCH
+const updateFilterOptions = (currentRecipes) => {
+    // INIT FILTER LISTS
+    const filterList = new FilterList(currentRecipes);
+    // Get lists
+    let ingredientList = filterList.getFilterLists(
+        currentRecipes,
+        'ingredients'
+    );
+    let applianceList = filterList.getFilterLists(currentRecipes, 'appliance');
+    let utensilsList = filterList.getFilterLists(currentRecipes, 'utensils');
+    // Filter lists
+    filterList.sortList(ingredientList);
+    filterList.sortList(applianceList);
+    filterList.sortList(utensilsList);
+    // Remove previous options
+    emptyHtmlElement('.ingredients__list');
+    emptyHtmlElement('.appliance__list');
+    emptyHtmlElement('.utensils__list');
+    // Display lists
+    filterList.displayList(ingredientList, 'ingredients');
+    filterList.displayList(applianceList, 'appliance');
+    filterList.displayList(utensilsList, 'utensils');
 };
 
 // MAIN BAR RESEARCH
@@ -18,10 +43,10 @@ export const enableMainResearch = () => {
     mainSearchBar.addEventListener('input', (e) => {
         const mainResearchString = e.target.value.toLowerCase();
         if (mainResearchString.length >= 3) {
-            console.log('On commence le tri !');
+            // Get recipes list
             const recipeListObject = new Recipe(recipes);
             let recipeList = recipeListObject.getRecipesList(recipes);
-            console.log(recipeList);
+            // Filter recipes list
             // TODO : search inside utensils.
             const filteredRecipes = recipeList.filter(
                 (elt) =>
@@ -30,10 +55,16 @@ export const enableMainResearch = () => {
                     elt.description.toLowerCase().includes(mainResearchString)
             );
             emptyHtmlElement('.results');
-            filteredRecipes.length !== 0
-                ? recipeListObject.displayRecipes(filteredRecipes)
-                : noResultMessage();
-            console.log(filteredRecipes);
+            // Display recipes on page or "no found" message.
+            if (filteredRecipes.length !== 0) {
+                recipeListObject.displayRecipes(filteredRecipes);
+                updateFilterOptions(filteredRecipes);
+            } else {
+                noResultMessage();
+                emptyHtmlElement('.ingredients__list');
+                emptyHtmlElement('.appliance__list');
+                emptyHtmlElement('.utensils__list');
+            }
         }
         console.log(mainResearchString.length);
     });
